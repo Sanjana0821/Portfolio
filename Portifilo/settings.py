8789140c6428bf401e1578ee5771181139451bbd
Sanjana <sanjanasnimbalkar821@gmail.com>
2026-06-29 09:50:36 +0530

    Prepare project for Vercel development

--- Portifilo/settings.py
+++ Portifilo/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d1446sj=_-0b8+6*()g)%q4xc1-+jj99qa(s&5mt&_ki7^tqro'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
